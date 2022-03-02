86439f48c63a33ec3a1200df60818ecfec3f6066
kernelk14 <khyleisaias@gmail.com>
2022-03-02 10:25:18 +0800

    Slug: Added a folder for tests

--- dsd.rs
+++
@@ -1,7 +0,0 @@
-use std::io;
-fn main() {
-  let num1 = 5;
-  let num2 = 6;
-  let num3 = num2 + num1;
-  println!("{}", num3);
-}
--- simp.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-fn main() {
-  let number = 5;
-  let number1 = 6;
-  let number3 = number1 + number;
-  let ano = 11;
-  let ann = 12;
-}
