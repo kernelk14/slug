e4ce8656eae8292dd4260fea95b4441d45ad25b4
kernelk14 <khyleisaias@gmail.com>
2022-05-27 07:07:47 +0800

    Slug: Added test files.

---
+++ tests/varass.rs
@@ -0,0 +1,10 @@
+#[allow(unused_variables, unused_assignments, unused_imports)]
+use std::io;
+fn main() {
+  let a = 10;
+  let b = 20;
+  let c = b + a;
+  println!("{}", a);
+  println!("{}", b);
+  println!("{}", c);
+}
