564b834d8b6d63ff109aef340aed0ff31454e23a
kernelk14 <khyleisaias@gmail.com>
2022-05-27 07:07:47 +0800

    Slug: Added test files.

---
+++ tests/for.rs
@@ -0,0 +1,11 @@
+#[allow(unused_variables, unused_assignments, unused_imports)]
+use std::io;
+fn main() {
+  let x = 5;
+  for (5..10 ){
+      println!("{}", x);
+  println!("{}", 10);
+  println!("{}", 11);
+
+  }
+}
